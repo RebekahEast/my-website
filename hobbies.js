868595c1c1fd30fa1a665b2fd6e51e71c396be5d
RebekahEast <rebekah.jean.east@gmail.com>
2023-01-27 10:26:09 +0000

    setting up JS framework for hobby to have see more option

--- hobbies.js
+++ hobbies.js
@@ -1,3 +1,5 @@
 const hobbies = document.getElementById("hobbies");
+const SeeMore = document.getElementById("load-more");
+const HobbyCount = document.getElementById("hobby-count");
+const HobbyTotal = document.getElementById("hobby-total");
 
-alert("Hello! I am an alert box!!");
