625d503cd20ff1b6d2d2718b0132b8c466c01bd4
RebekahEast <rebekah.jean.east@gmail.com>
2023-03-06 16:54:04 +0000

    testing out a new pet page style

--- pets.js
+++ pets.js
@@ -12,3 +12,23 @@ function removeActiveClasses() {
         panel.classList.remove('active')
     })
 }
+
+
+////////
+
+const pets = [
+    {
+        name: "Pheobe",
+        age: "beautiful Pheeb is coming up on 8 years old now - reaching golden oldie territory",
+        likes: "playing in the park and chasing Leo with the ball. Belly rubs and cuddles",
+        dislikes: "getting groomed and not being able to cuddle people who make eye contact with her",
+        hobbies: "toys. toys. sleep. cuddle. toys.",
+    }, 
+    {
+        name: "Pheobe",
+        age: "beautiful Pheeb is coming up on 8 years old now - reaching golden oldie territory",
+        likes: "playing in the park and chasing Leo with the ball. Belly rubs and cuddles",
+        dislikes: "getting groomed and not being able to cuddle people who make eye contact with her",
+        hobbies: "toys. toys. sleep. cuddle. toys.",
+    }
+]
