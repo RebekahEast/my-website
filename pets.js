d7ac9c893efb17b6a1f4fa103bbd70f2eb3064a6
RebekahEast <rebekah.jean.east@gmail.com>
2023-03-06 17:05:05 +0000

    calling the array isn't working right now

--- pets.js
+++ pets.js
@@ -23,12 +23,36 @@ const pets = [
         likes: "playing in the park and chasing Leo with the ball. Belly rubs and cuddles",
         dislikes: "getting groomed and not being able to cuddle people who make eye contact with her",
         hobbies: "toys. toys. sleep. cuddle. toys.",
+        nicknames: "Spleeb, Phoebert, Smeller Ella"
     }, 
     {
-        name: "Pheobe",
-        age: "beautiful Pheeb is coming up on 8 years old now - reaching golden oldie territory",
-        likes: "playing in the park and chasing Leo with the ball. Belly rubs and cuddles",
-        dislikes: "getting groomed and not being able to cuddle people who make eye contact with her",
-        hobbies: "toys. toys. sleep. cuddle. toys.",
-    }
-]
+        name: "Leo",
+        age: "acts like a full baby, is actually 4",
+        likes: "playing in the park and having Phoebe chase him. Leo loves food",
+        dislikes: "being left on his own (especially without Pheeb!)",
+        hobbies: "tennis balls for days! Cuddling up to people and giving no personal space",
+        nicknames: "stinker, Leroy, Leonardo"
+    },
+    {
+        name: "Juniper Gin",
+        age: "born on April Fool's Day 2019",
+        likes: "likes to lounge around all day in the sunshine, and eat her favourite snack - dreamies.",
+        dislikes: "Anything loud and unfamiliar. Being told no. Any snacks not dreamies",
+        hobbies: "Juniper likes to play string toys, eat food, and wake her pawrents up at 4am every morning.",
+        nicknames: "Tuniper Tin, Junebug, LovelyRug"
+    },
+];
+
+let firstArray = 0;
+
+let totalPets = pets.length;
+
+let PetsContainer = document.getElementById("pet-container");
+let ClickNext = document.getElementById("next");
+let ClickBack = document.getElementById("previous");
+
+let displayPets = () => {
+    PetsContainer.innerHTML = `
+        <p>${pets[firstArray].likes}</p>
+    `
+}
