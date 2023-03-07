4b9566eb432a9e9193ee8f137fe6aaac8eed5a26
RebekahEast <rebekah.jean.east@gmail.com>
2023-03-07 14:19:43 +0000

    image in the array now

--- pets.js
+++ pets.js
@@ -24,7 +24,7 @@ const pets = [
         dislikes: "getting groomed and not being able to cuddle people who make eye contact with her",
         hobbies: "toys. toys. sleep. cuddle. toys.",
         nicknames: "Spleeb, Phoebert, Smeller Ella",
-        image: "Pheobe 1.JPG" //how to have this as an image?//
+        images: '../pictures/Pheobe2.JPG',
     }, 
     {
         name: "Leo",
@@ -32,7 +32,8 @@ const pets = [
         likes: "playing in the park and having Phoebe chase him. Leo loves food",
         dislikes: "being left on his own (especially without Pheeb!)",
         hobbies: "tennis balls for days! Cuddling up to people and giving no personal space",
-        nicknames: "stinker, Leroy, Leonardo"
+        nicknames: "stinker, Leroy, Leonardo",
+        images: '../pictures/Leo1.jpg',
     },
     {
         name: "Juniper Gin",
@@ -40,7 +41,8 @@ const pets = [
         likes: "likes to lounge around all day in the sunshine, and eat her favourite snack - dreamies.",
         dislikes: "Anything loud and unfamiliar. Being told no. Any snacks not dreamies",
         hobbies: "Juniper likes to play string toys, eat food, and wake her pawrents up at 4am every morning.",
-        nicknames: "Tuniper Tin, Junebug, LovelyRug"
+        nicknames: "Tuniper Tin, Junebug, LovelyRug",
+        images: '../pictures/Juniper2.jpg',
     },
 ];
 
@@ -61,7 +63,7 @@ let displayPets = () => {
             Dislikes: ${pets[firstArray].dislikes}<br><br>
             Hobbies: ${pets[firstArray].hobbies}<br><br>
             Nicknames: ${pets[firstArray].nicknames}<br><br>
-            <img src=${pets[firstArray].image}>
+            <img src=${pets[firstArray].images}>
         </p>
     `;
 };
